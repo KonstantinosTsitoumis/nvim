a9548e0cf9f81e4a7313e267ab9d9945aba8efeb
KonstantinosTsitoumis <k.tsitoumis@withplum.com>
2024-10-19 14:02:17 +0300

    add git link yank, change colorscheme

--- lua/tsitVim/plugins/colorscheme.lua
+++ lua/tsitVim/plugins/colorscheme.lua
@@ -76,4 +76,18 @@ local gruvbox = {
 	end,
 	opts = ...,
 }
-return tokyon
+
+local cyberdream = {
+	"scottmckendry/cyberdream.nvim",
+	lazy = false,
+	priority = 1000,
+	config = function()
+		require("cyberdream").setup({
+			transparent = true,
+		})
+
+		vim.cmd("colorscheme cyberdream")
+	end,
+}
+
+return cyberdream
